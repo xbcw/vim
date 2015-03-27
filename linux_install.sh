c3eed35a2a7e1a588cfbe964da0d38de4729872a
Brad Williams <bwilliams@ics-llc.net>
2015-03-27 11:07:46 -0500

    added --full-install flag to do time consuming installation steps such as compiling binaries

--- linux_install.sh
+++ linux_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-cp .vimrc ~/.vimrc
-cp -rp autoload $HOME/.vim/
-cp -rp colors $HOME/.vim/
-cp -rp bundle $HOME/.vim/
+cp .vimrc $HOME/.vimrc
+mkdir $HOME/.vim
+cp -r autoload $HOME/.vim
+cp -r bundle $HOME/.vim
 
-# Finish setting up YouCompleteMe
-sudo apt-get install build-essentials cmake python-dev
-bash $HOME/.vim/bundle/YouCompleteMe/install.sh
+# Do a full install
+if [ "$1" == "--full-install" ] || [ "$1" == "-f" ] ; then 
+    # Finish setting up YouCompleteMe
+    sudo apt-get install build-essential cmake python-dev;
+    bash $HOME/.vim/bundle/YouCompleteMe/install.sh;
+fi
